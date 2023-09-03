a34dc5321e3ae8a2e931049a8ec288189b6898f1
IvanOrsh <ksironov@mail.ru>
2023-09-03 22:57:57 +0300

    feat: make use of user store

--- api/user_handler.go
+++ api/user_handler.go
@@ -1,11 +1,38 @@
 package api
 
 import (
+	"context"
+
+	"github.com/IvanOrsh/go-hot-res/db"
 	"github.com/IvanOrsh/go-hot-res/types"
+
 	"github.com/gofiber/fiber/v2"
 )
 
-func HandleGetUsers(c *fiber.Ctx) error {
+type UserHandler struct {
+	userStore db.UserStore
+}
+
+func NewUserHandler(userStore db.UserStore) *UserHandler {
+	return &UserHandler{
+		userStore: userStore,
+	}
+}
+
+func (h *UserHandler) HandleGetUser(c *fiber.Ctx) error {
+	var (
+		id = c.Params("id")
+		ctx = context.Background()
+	)
+	user, err := h.userStore.GetUserByID(ctx, id)
+	if err != nil {
+		return err
+	}
+	
+	return c.JSON(user)
+}
+
+func (h *UserHandler) HandleGetUsers(c *fiber.Ctx) error {
 	u := types.User{
 		FirstName: "James",
 		LastName: "St James",
@@ -13,6 +40,3 @@ func HandleGetUsers(c *fiber.Ctx) error {
 	return c.JSON(u)
 }
 
-func HandleGetUser(c *fiber.Ctx) error {
-	return c.JSON("James")
-}
