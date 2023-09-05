73f3c56942335a78d0a0acb7b6fc53ed4dc32b62
IvanOrsh <ksironov@mail.ru>
2023-09-05 22:10:54 +0300

    test: get user handler

--- api/user_handler_test.go
+++ api/user_handler_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http/httptest"
 	"testing"
@@ -11,6 +12,7 @@ import (
 	"github.com/IvanOrsh/go-hot-res/db"
 	"github.com/IvanOrsh/go-hot-res/types"
 	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -106,6 +108,49 @@ func TestPostUser(t *testing.T) {
 	
 	if user.Email != params.Email {
 		t.Errorf("expected email %s but got %s", params.Email, user.Email)
+	}
+}
+
+func TestGetUser(t *testing.T) {
+	tdb := setup(t)
+	insertedUser := tdb.seedUsers(t)
+	defer tdb.teardown(t)
+
+	app := fiber.New()
+	userHandler := NewUserHandler(tdb.UserStore)
+	app.Get("/:id", userHandler.HandleGetUser)
+
+	stringObjectID := primitive.ObjectID.Hex(insertedUser.ID)
+
+	req := httptest.NewRequest(
+		"GET",
+		fmt.Sprintf("/%s", stringObjectID),
+		nil,
+	)
+	req.Header.Set("Content-Type", "application/json")
+	resp, err := app.Test(req)
+	if err != nil {
+		t.Error(err)
+	}
+	
+	var user types.User
+
+	json.NewDecoder(resp.Body).Decode(&user)
+
+	if resp.StatusCode != 200 {
+		t.Errorf("expected status code to be 200")
+	}	
+
+	if user.FirstName != insertedUser.FirstName {
+		t.Errorf("expected firstName %s but got %s", user.FirstName, insertedUser.FirstName)
+	}
+
+	if user.LastName != insertedUser.LastName {
+		t.Errorf("expected lastName %s but got %s", user.LastName, insertedUser.LastName)
+	}
+	
+	if user.Email != insertedUser.Email {
+		t.Errorf("expected email %s but got %s", user.Email, insertedUser.Email)
 	}
 }
 
