be756761fe798a4d376ce33aba9be72e9d30a671
IvanOrsh <ksironov@mail.ru>
2023-09-06 03:28:56 +0300

    chore: script to seed db with hotels & rooms

---
+++ scripts/seed.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/IvanOrsh/go-hot-res/db"
+	"github.com/IvanOrsh/go-hot-res/types"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var (
+	client     *mongo.Client
+	roomStore  db.RoomStore
+	hotelStore db.HotelStore
+	ctx        context.Context
+)
+
+func init() {
+	ctx = context.Background()
+	var err error
+	client, err = mongo.Connect(context.TODO(), options.Client().ApplyURI(db.DBURI))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// drop
+	if err := client.Database(db.DBNAME).Drop(ctx); err != nil {
+		log.Fatal(err)
+	}
+
+	hotelStore = db.NewMongoHotelStore(client)
+	roomStore = db.NewMongoRoomStore(client, hotelStore)
+}
+
+func seedHotel(name string, location string, rating int) {
+	hotel := types.Hotel{
+		Name:     name,
+		Location: location,
+		Rating: rating,
+		Rooms:    []primitive.ObjectID{},
+	}
+
+	rooms := []types.Room{
+		{
+			Type:      types.SingleRoomType,
+			BasePrice: 99.9,
+		},
+		{
+			Type:      types.DeluxeRoomType,
+			BasePrice: 199.9,
+		},
+		{
+			Type:      types.SeaSideRoomType,
+			BasePrice: 122.9,
+		},
+	}
+
+	insertedHotel, err := hotelStore.InsertHotel(ctx, &hotel)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(insertedHotel)
+
+	for _, room := range rooms {
+		room.HotelID = insertedHotel.ID
+		insertedRoom, err := roomStore.InsertRoom(ctx, &room)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(insertedRoom)
+	}
+}
+
+func main() {
+	seedHotel("Bellucia", "France", 3)
+	seedHotel("The cozy hotel", "The Netherlands", 4)
+	seedHotel("Don't die in your sleep", "London", 1)
+}
